cd3839abecc020ec0d2baa339eda0719b3758b6d
Marat Galiev <kazanlug@gmail.com>
2024-06-28 21:56:09 -0400

    Initial Commit

---
+++ browsera/browseraApp.swift
@@ -0,0 +1,17 @@
+//
+//  browseraApp.swift
+//  browsera
+//
+//  Created by Marat Galiev on 2024-06-28.
+//
+
+import SwiftUI
+
+@main
+struct browseraApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
